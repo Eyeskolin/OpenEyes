5d0cd29b537ee9a30da169fbcb5c7791d1b5aef4
zpc <1378674684@qq.com>
2017-12-27 20:17:16 +0800

    Frament 抽取基类

--- Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/fragmnet/My.kt
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/fragmnet/My.kt
@@ -1,18 +1,35 @@
 package com.zpc.kolin_eyes.fragmnet
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
+import android.content.Intent
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import com.zpc.kolin_eyes.R
+import com.zpc.kolin_eyes.avtivity.BaseFragment
+import com.zpc.kolin_eyes.avtivity.SettingActivity
+import com.zpc.kolin_eyes.presenter.MinePrestener
+import kotlinx.android.synthetic.main.my_fra.*
 
 /**
  * Created by DELL on 2017/12/27.
  */
-class My :Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view :View?=inflater?.inflate(R.layout.my_fra,container,false)
-        return view
+class My :BaseFragment<MinePrestener>() {
+    override fun getLayoutId(): Int = R.layout.my_fra
+
+    override fun initView(view: View?) {
+        sett.setOnClickListener{
+            val intent=Intent(activity,SettingActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(activity,"点击了",Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    override fun getPrestener(): MinePrestener {
+        var minePrestener :MinePrestener = MinePrestener()
+        return minePrestener
+    }
+
+    override fun initData() {
+    }
+
 }
