bab9719e3839d2c337510c6380743011d2615b57
zpc <1378674684@qq.com>
2017-12-27 14:48:06 +0800

    aaa

--- Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/net/Apii.kt
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/net/Apii.kt
@@ -4,5 +4,8 @@ package com.zpc.kolin_eyes.net
  * Created by DELL on 2017/12/27.
  */
 class Apii {
-
+    //接口地址
+    companion object {
+        val BASE_URL = "http://baobab.wandoujia.com/api/v2/"
+    }
 }
---
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/net/RetrofitUtil.kt
@@ -0,0 +1,8 @@
+package com.zpc.kolin_eyes.net
+
+/**
+ * Created by DELL on 2017/12/27.
+ */
+class RetrofitUtil {
+
+}
