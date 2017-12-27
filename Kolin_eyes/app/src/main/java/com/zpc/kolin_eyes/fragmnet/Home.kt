140712a8116e68f68089f08f8459392acd887e0e
FLOWER <984659607@qq.com>
2017-12-27 21:03:41 +0800

    请求首页数据

--- Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/fragmnet/Home.kt
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/fragmnet/Home.kt
@@ -1,18 +1,37 @@
 package com.zpc.kolin_eyes.fragmnet
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import com.zpc.kolin_eyes.R
+import com.zpc.kolin_eyes.avtivity.BaseFragment
+import com.zpc.kolin_eyes.bean.Homeinfo
+import com.zpc.kolin_eyes.presenter.Hpresenter
+import com.zpc.kolin_eyes.view.HVIew
 
 /**
  * Created by DELL on 2017/12/27.
  */
-class Home :Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view :View?=inflater?.inflate(R.layout.home_fra,container,false)
-        return view
+class Home : BaseFragment<Hpresenter>(),HVIew{
+    override fun getLayoutId(): Int {
+        return R.layout.home_fra
     }
+
+    override fun initView(view: View?) {
+
+    }
+
+    override fun getPrestener(): Hpresenter {
+        var hpresenter : Hpresenter =Hpresenter(this)
+        return hpresenter
+    }
+
+    override fun initData() {
+        prestener!!.relevance()
+    }
+
+    override fun getData(homeinfo: Homeinfo) {
+        val pageUrl = homeinfo.nextPageUrl
+        Log.i("xxx",pageUrl.toString())
+    }
+
 }
--- Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/net/ApiService.kt
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/net/ApiService.kt
@@ -1,8 +1,16 @@
 package com.zpc.kolin_eyes.net
 
+import com.zpc.kolin_eyes.bean.Homeinfo
+import io.reactivex.Flowable
+import retrofit2.http.GET
+
 /**
  * Created by DELL on 2017/12/27.
  */
 interface ApiService {
+
+    //获取首页第一页数据
+    @GET("feed?num=2&udid=26868b32e808498db32fd51fb422d00175e179df&vc=83")
+    fun gethomeinfo():Flowable<Homeinfo>
 
 }
