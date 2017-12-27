45350d148e75aa2567abb851c288a209cf14d09b
FLOWER <984659607@qq.com>
2017-12-27 15:36:12 +0800

    分包

---
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/model/HModel.kt
@@ -0,0 +1,7 @@
+package com.zpc.kolin_eyes.model
+
+/**
+ * Created by FLOWER on 2017/12/27.
+ */
+interface HModel {
+}
---
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/view/HVIew.kt
@@ -0,0 +1,7 @@
+package com.zpc.kolin_eyes.view
+
+/**
+ * Created by FLOWER on 2017/12/27.
+ */
+interface HVIew {
+}
