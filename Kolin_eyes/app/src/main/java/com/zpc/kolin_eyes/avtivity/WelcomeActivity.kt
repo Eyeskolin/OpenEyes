8203b69d7167c16815e4b86fcdf70ce488ac6f93
zpc <1378674684@qq.com>
2017-12-29 19:14:51 +0800

    daf

--- Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/avtivity/WelcomeActivity.kt
+++ Kolin_eyes/app/src/main/java/com/zpc/kolin_eyes/avtivity/WelcomeActivity.kt
@@ -15,34 +15,40 @@ class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //设置为全屏
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_welcome)
         startAnim()
     }
 
     private fun startAnim() {
+        //透明动画
         val alphaAnimation = AlphaAnimation(0.1f,1.0f)
+        //动画执行时间
         alphaAnimation.duration = 1000
+        //缩放动画
         val scaleAnimation = ScaleAnimation(0.1f,1.0f,0.1f,1.0f, ScaleAnimation.RELATIVE_TO_SELF,0.5f, ScaleAnimation.RELATIVE_TO_SELF,0.5f)
+        //动画执行时间
         scaleAnimation.duration =1000
+        //动画集合
         val animationSet = AnimationSet(true)
         animationSet.addAnimation(alphaAnimation)
         animationSet.addAnimation(scaleAnimation)
         animationSet.duration = 1000
+        //启动动画
         wel_img.startAnimation(animationSet)
+        //设置动画监听
         animationSet.setAnimationListener(object :Animation.AnimationListener{
             override fun onAnimationRepeat(animation: Animation?) {
             }
-
             override fun onAnimationEnd(animation: Animation?) {
+                //动画结束时跳转
                 var intent = Intent(this@WelcomeActivity,MainActivity::class.java)
                 startActivity(intent)
                 finish()
             }
-
             override fun onAnimationStart(animation: Animation?) {
             }
-
         })
     }
 }
